69b4bfb7191a6fa2912a936d4cf588c0ecb266b9
FurkanKepenek <h.furkankepenek@gmail.com>
2022-07-07 09:42:09 +0300

    new commit

--- R/hgncConverter2.R
+++ R/hgncConverter2.R
@@ -8,29 +8,30 @@
 #' @param colname Name of the column containing gene synonyms to be converted to gene names
 #' @return dataframe object
 #' @export
+#' @import dplyr
 #'
 hgncConverter2<-function(genelist, colname){
 
   colname2<-"Gene_name"
   colname3<-"Genename"
   tempcolname<-"Gene_synonyms"
-  approved<-semi_join(genelist, human_hgnc, by = setNames(colname2, colname))
-  someWOapproved<-anti_join(genelist, approved, by = colname)
-  notApproved<-semi_join(someWOapproved, human_hgnc, by = setNames(tempcolname, colname))
+  approved<-dplyr::semi_join(genelist, human_hgnc, by = setNames(colname2, colname))
+  someWOapproved<-dplyr::anti_join(genelist, approved, by = colname)
+  notApproved<-dplyr::semi_join(someWOapproved, human_hgnc, by = setNames(tempcolname, colname))
   some<-rbind(approved, notApproved)
-  non<-anti_join(genelist, some, by = colname)
+  non<-dplyr::anti_join(genelist, some, by = colname)
 
   if ("Gene_name" %in% colnames(genelist) & !("Genename" %in% colnames(genelist))){
     hgnc_temp<-human_hgnc
     colnames(hgnc_temp)[1]<-"Genename"
-    temp_notApproved<-left_join(notApproved, hgnc_temp, by = setNames(tempcolname, colname))
+    temp_notApproved<-dplyr::left_join(notApproved, hgnc_temp, by = setNames(tempcolname, colname))
     temp_notApproved[[colname]]<-temp_notApproved$Genename
-    temp_notApproved<-temp_notApproved %>% select(!Genename)
+    temp_notApproved<-temp_notApproved %>% dplyr::select(!Genename)
   }
   else if (!("Gene_name" %in% colnames(genelist))){
-    temp_notApproved<-left_join(notApproved, human_hgnc, by = setNames(tempcolname, colname))
+    temp_notApproved<-dplyr::left_join(notApproved, human_hgnc, by = setNames(tempcolname, colname))
     temp_notApproved[[colname]]<-temp_notApproved$Gene_name
-    temp_notApproved<-temp_notApproved %>% select(!Gene_name)
+    temp_notApproved<-temp_notApproved %>% dplyr::select(!Gene_name)
   }
   else {
     stop("Change the name of that damn column!")
@@ -40,3 +41,5 @@ hgncConverter2<-function(genelist, colname){
   return(all)
 
 }
+
+
